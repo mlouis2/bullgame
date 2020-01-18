1a898c72abae07737c739e9ca80d6cf92ac423fd
Merissa Tan <merissa030397@gmail.com>
2020-01-18 13:30:16 -0800

    Add canvas resizing

--- main.js
+++ main.js
@@ -3,9 +3,16 @@ const ctx = canvas.getContext("2d");
 
 function setBackground() {
   ctx.fillStyle = "#123456";
-  canvas.width = window.innerWidth * 0.8;
-  canvas.height = window.innerWidth * 0.8 * 0.75;
-  console.log("canvas width is " + canvas.width);
+  const defaultCanvasWidth = window.innerWidth * 0.8;
+  canvas.width = defaultCanvasWidth;
+  const adjustedCanvasHeight = defaultCanvasWidth / 16 * 9; // 16:9 aspect ratio
+  if (window.innerHeight < adjustedCanvasHeight) {
+    const defaultCanvasHeight = window.innerHeight * 0.9;
+    canvas.height = defaultCanvasHeight;
+    canvas.width = defaultCanvasHeight * 16 / 9;
+  } else {
+    canvas.height = adjustedCanvasHeight;
+  }
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
