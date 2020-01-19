8593bcb56a8cbe9686f30c999b322a20597f48ab
Merissa Tan <merissa030397@gmail.com>
2020-01-19 09:36:02 -0800

    add level two

--- levelLoader.js
+++ levelLoader.js
@@ -12,27 +12,22 @@ function loadLevelOne() {
   request.open("GET", "./gameFiles/levelOneCols.json", false);
   request.send(null);
   levelOneInfo = JSON.parse(request.responseText);
-  console.log("pushing level one info");
   levelInfo.push(levelOneInfo);
 }
 
 function loadLevelTwo() {
   let request = new XMLHttpRequest();
-  request.open("GET", "./gameFiles/levelOneCols.json", false);
+  request.open("GET", "./gameFiles/levelTwoCols.json", false);
   request.send(null);
-  levelOneInfo = JSON.parse(request.responseText);
-  console.log("pushing fake level two info");
-  levelInfo.push(levelOneInfo);
+  levelTwoInfo = JSON.parse(request.responseText);
+  levelInfo.push(levelTwoInfo);
 }
 
 function getDoorLocationAtLevel(level) {
-  console.log(levelInfo);
-  console.log(level - 1);
   return levelInfo[level - 1].doorLocation;
 }
 
 function getPlayerStartLocationAtLevel(level) {
-  console.log("getting this");
   return levelInfo[level - 1].playerStartLocation;
 }
 
